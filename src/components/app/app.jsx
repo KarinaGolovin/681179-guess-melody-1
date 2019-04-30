785be6e36bee7d339d33ccf258347dd80e0e864f
Karina Golovin <karina.golovin@gmail.com>
2019-04-30 15:12:59 +0300

    Added protoType check

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+
 
 const App = (props) => {
   const {gameTime, errorCount} = props;
@@ -8,6 +10,11 @@ const App = (props) => {
     time = {gameTime}
     errorCount = {errorCount}
   />;
+};
+
+App.propTypes = {
+  gameTime: PropTypes.number,
+  errorCount: PropTypes.number
 };
 
 export default App;
